acd742bd9cac192b6552e6e6c8fdd266553d3dbe
Om0423 <78515066+Om0423@users.noreply.github.com>
2021-07-06 17:25:51 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+import dictionary from "./database";
+var word = dictionary[text]["word"]
+var lexicalCategory = dictionary[text]["lexicalCategory"]
+var definition = dictionary[text]["definition"]
+
+this.setState({
+"word":word,
+"lexicalCategory": lexicalCategory,
+"definition":definition
+})
